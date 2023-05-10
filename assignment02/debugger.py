c35503059f779ddd959bcc65a509b73a5cf56ccd
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-05-10 16:02:00 +0900

    a02: copy Tracer

--- assignment02/debugger.py
+++ assignment02/debugger.py
@@ -1,8 +1,9 @@
 from types import FrameType, FunctionType, TracebackType
-from typing import cast, Any, Optional, Callable, Type
+from typing import cast, Any, Optional, Callable, Type, TextIO
 import inspect
 import warnings
 import traceback
+import sys
 
 Location = tuple[Callable, int]
 
@@ -123,3 +124,103 @@ class StackInspector:
                 return True
 
         return False
+
+
+class Tracer(StackInspector):
+    def __init__(self, *, file: TextIO = sys.stdout) -> None:
+        """Trace a block of code, sending logs to `file` (default: stdout)"""
+        self.original_trace_function: Optional[Callable] = None
+        self.file = file
+        self.last_vars: dict[str, Any] = {}
+
+    def traceit(self, frame: FrameType, event: str, arg: Any) -> None:
+        """Tracing function; called at every line. To be overloaded in subclasses."""
+        self.print_debugger_status(frame, event, arg)
+
+    def _traceit(self, frame: FrameType, event: str, arg: Any) -> Optional[Callable]:
+        """Internal tracing function."""
+        if self.our_frame(frame):
+            # Do not trace our own methods
+            pass
+        else:
+            self.traceit(frame, event, arg)
+        return self._traceit
+
+    def log(self, *objects: Any,
+            sep: str = ' ', end: str = '\n',
+            flush: bool = True) -> None:
+        """
+        Like `print()`, but always sending to `file` given at initialization,
+        and flushing by default.
+        """
+        print(*objects, sep=sep, end=end, file=self.file, flush=flush)
+
+    def __enter__(self) -> Any:
+        """Called at begin of `with` block. Turn tracing on."""
+        self.original_trace_function = sys.gettrace()
+        sys.settrace(self._traceit)
+
+        # This extra line also enables tracing for the current block
+        # inspect.currentframe().f_back.f_trace = self._traceit
+        return self
+
+    def __exit__(self, exc_tp: Type, exc_value: BaseException,
+                 exc_traceback: TracebackType) -> Optional[bool]:
+        """
+        Called at end of `with` block. Turn tracing off.
+        Return `None` if ok, not `None` if internal error.
+        """
+        sys.settrace(self.original_trace_function)
+
+        # Note: we must return a non-True value here,
+        # such that we re-raise all exceptions
+        if self.is_internal_error(exc_tp, exc_value, exc_traceback):
+            return False  # internal error
+        else:
+            return None  # all ok
+
+    def changed_vars(self, new_vars: dict[str, Any]) -> dict[str, Any]:
+        """Track changed variables, based on `new_vars` observed."""
+        changed = {}
+        for var_name, var_value in new_vars.items():
+            if (var_name not in self.last_vars or
+                    self.last_vars[var_name] != var_value):
+                changed[var_name] = var_value
+        self.last_vars = new_vars.copy()
+        return changed
+
+    def print_debugger_status(self, frame: FrameType, event: str, arg: Any) -> None:
+        """Show current source line and changed vars"""
+        if event == 'exception':
+            exception, value, tb = arg
+            self.log(f"{frame.f_code.co_name}() "
+                     f"raises {exception.__name__}: {value}")
+            return
+
+        changes = self.changed_vars(frame.f_locals)
+        changes_s = ", ".join([var + " = " + repr(changes[var])
+                               for var in changes])
+
+        if event == 'call':
+            self.log("Calling " + frame.f_code.co_name + '(' + changes_s + ')')
+        elif changes:
+            self.log(' ' * 40, '#', changes_s)
+
+        if event == 'line':
+            try:
+                module = inspect.getmodule(frame.f_code)
+                if module is None:
+                    source = inspect.getsource(frame.f_code)
+                else:
+                    source = inspect.getsource(module)
+                current_line = source.split('\n')[frame.f_lineno - 1]
+
+            except OSError as err:
+                self.log(f"{err.__class__.__name__}: {err}")
+                current_line = ""
+
+            self.log(repr(frame.f_lineno) + ' ' + current_line)
+
+        if event == 'return':
+            self.log(frame.f_code.co_name + '()' + " returns " + repr(arg))
+            self.last_vars = {}  # Delete 'last' variables
