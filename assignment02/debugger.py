1f83f9cc54a3743ba9ba1081d307850be64f7f24
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-05-15 09:39:28 +0900

    a02: fix type checking issue

--- assignment02/debugger.py
+++ assignment02/debugger.py
@@ -1,11 +1,13 @@
-from types import FrameType, FunctionType, TracebackType
-from typing import Any, Optional, Callable, Type, TextIO
 import inspect
-import warnings
-import traceback
 import sys
+import traceback
+import warnings
+from types import FrameType, FunctionType, TracebackType
+from typing import Any, Callable, Optional, TextIO, Type, TypeVar
 
-Location = tuple[Callable, int]
+AnyCallable = Callable[..., Any]
+BaseExceptionT = TypeVar("BaseExceptionT", bound=BaseException)
+Location = tuple[AnyCallable, int]
 
 
 class StackInspector:
@@ -26,7 +28,7 @@ class StackInspector:
 
     def our_frame(self, frame: FrameType) -> bool:
         """Return true if `frame` is in the current (inspecting) class."""
-        return isinstance(frame.f_locals.get('self'), self.__class__)
+        return isinstance(frame.f_locals.get("self"), self.__class__)
 
     def caller_globals(self) -> dict[str, Any]:
         """Return the globals() environment of the caller."""
@@ -40,8 +42,11 @@ class StackInspector:
         """Return the location (func, lineno) of the caller."""
         return self.caller_function(), self.caller_frame().f_lineno
 
-    def search_frame(self, name: str, frame: Optional[FrameType] = None) -> \
-        tuple[Optional[FrameType], Optional[Callable]]:
+    def search_frame(
+        self,
+        name: str,
+        frame: Optional[FrameType] = None,
+    ) -> tuple[Optional[FrameType], Optional[AnyCallable]]:
         """
         Return a pair (`frame`, `item`)
         in which the function `name` is defined as `item`.
@@ -62,16 +67,15 @@ class StackInspector:
 
         return None, None
 
-    def search_func(self, name: str, frame: Optional[FrameType] = None) -> \
-        Optional[Callable]:
+    def search_func(self, name: str, frame: Optional[FrameType] = None) -> Optional[AnyCallable]:
         """Search in callers for a definition of the function `name`"""
         frame, func = self.search_frame(name, frame)
         return func
 
     # Avoid generating functions more than once
-    _generated_function_cache: dict[tuple[str, int], Callable] = {}
+    _generated_function_cache: dict[tuple[str, int], AnyCallable] = {}
 
-    def create_function(self, frame: FrameType) -> Callable:
+    def create_function(self, frame: FrameType) -> AnyCallable:
         """Create function for given frame"""
         name = frame.f_code.co_name
         cache_key = (name, frame.f_lineno)
@@ -80,9 +84,7 @@ class StackInspector:
 
         try:
             # Create new function from given code
-            generated_function = FunctionType(frame.f_code,
-                                                   globals=frame.f_globals,
-                                                   name=name)
+            generated_function = FunctionType(frame.f_code, globals=frame.f_globals, name=name)
         except TypeError:
             # Unsuitable code for creating a function
             # Last resort: Return some function
@@ -90,14 +92,15 @@ class StackInspector:
 
         except Exception as exc:
             # Any other exception
-            warnings.warn(f"Couldn't create function for {name} "
-                          f" ({type(exc).__name__}: {exc})")
+            warnings.warn(
+                f"Couldn't create function for {name} ({type(exc).__name__}: {exc})"
+            )
             generated_function = self.unknown
 
         self._generated_function_cache[cache_key] = generated_function
         return generated_function
 
-    def caller_function(self) -> Callable:
+    def caller_function(self) -> AnyCallable:
         """Return the calling function"""
         frame = self.caller_frame()
         name = frame.f_code.co_name
@@ -105,7 +108,7 @@ class StackInspector:
         if func:
             return func
 
-        if not name.startswith('<'):
+        if not name.startswith("<"):
             warnings.warn(f"Couldn't find {name} in caller")
 
         return self.create_function(frame)
@@ -113,14 +116,17 @@ class StackInspector:
     def unknown(self) -> None:  # Placeholder for unknown functions
         pass
 
-    def is_internal_error(self, exc_tp: Type,
-                          exc_value: BaseException,
-                          exc_traceback: TracebackType) -> bool:
+    def is_internal_error(
+        self,
+        exc_tp: Type[BaseExceptionT],
+        exc_value: BaseExceptionT,
+        exc_traceback: TracebackType,
+    ) -> bool:
         """Return True if exception was raised from `StackInspector` or a subclass."""
         if not exc_tp:
             return False
 
-        for frame, lineno in traceback.walk_tb(exc_traceback):
+        for frame, _ in traceback.walk_tb(exc_traceback):
             if self.our_frame(frame):
                 return True
 
@@ -130,7 +136,7 @@ class StackInspector:
 class Tracer(StackInspector):
     def __init__(self, *, file: TextIO = sys.stdout) -> None:
         """Trace a block of code, sending logs to `file` (default: stdout)"""
-        self.original_trace_function: Optional[Callable] = None
+        self.original_trace_function: Optional[AnyCallable] = None
         self.file = file
         self.last_vars: dict[str, Any] = {}
 
@@ -138,7 +144,7 @@ class Tracer(StackInspector):
         """Tracing function; called at every line. To be overloaded in subclasses."""
         self.print_debugger_status(frame, event, arg)
 
-    def _traceit(self, frame: FrameType, event: str, arg: Any) -> Optional[Callable]:
+    def _traceit(self, frame: FrameType, event: str, arg: Any) -> Optional[AnyCallable]:
         """Internal tracing function."""
         if self.our_frame(frame):
             # Do not trace our own methods
@@ -147,9 +153,7 @@ class Tracer(StackInspector):
             self.traceit(frame, event, arg)
         return self._traceit
 
-    def log(self, *objects: Any,
-            sep: str = ' ', end: str = '\n',
-            flush: bool = True) -> None:
+    def log(self, *objects: Any, sep: str = " ", end: str = "\n", flush: bool = True) -> None:
         """
         Like `print()`, but always sending to `file` given at initialization,
         and flushing by default.
@@ -165,8 +169,12 @@ class Tracer(StackInspector):
         # inspect.currentframe().f_back.f_trace = self._traceit
         return self
 
-    def __exit__(self, exc_tp: Type, exc_value: BaseException,
-                 exc_traceback: TracebackType) -> bool:
+    def __exit__(
+        self,
+        exc_tp: Type[BaseExceptionT],
+        exc_value: BaseExceptionT,
+        exc_traceback: TracebackType,
+    ) -> bool:
         """
         Called at end of `with` block. Turn tracing off.
         Return `None` if ok, not `None` if internal error.
@@ -176,48 +184,45 @@ class Tracer(StackInspector):
 
     def changed_vars(self, new_vars: dict[str, Any]) -> dict[str, Any]:
         """Track changed variables, based on `new_vars` observed."""
-        changed = {}
+        changed: dict[str, Any] = {}
         for var_name, var_value in new_vars.items():
-            if (var_name not in self.last_vars or
-                    self.last_vars[var_name] != var_value):
+            if var_name not in self.last_vars or self.last_vars[var_name] != var_value:
                 changed[var_name] = var_value
         self.last_vars = new_vars.copy()
         return changed
 
     def print_debugger_status(self, frame: FrameType, event: str, arg: Any) -> None:
         """Show current source line and changed vars"""
-        if event == 'exception':
-            exception, value, tb = arg
-            self.log(f"{frame.f_code.co_name}() "
-                     f"raises {exception.__name__}: {value}")
+        if event == "exception":
+            exception, value, _ = arg
+            self.log(f"{frame.f_code.co_name}() " f"raises {exception.__name__}: {value}")
             return
 
         changes = self.changed_vars(frame.f_locals)
-        changes_s = ", ".join([var + " = " + repr(changes[var])
-                               for var in changes])
+        changes_s = ", ".join([var + " = " + repr(changes[var]) for var in changes])
 
-        if event == 'call':
-            self.log("Calling " + frame.f_code.co_name + '(' + changes_s + ')')
+        if event == "call":
+            self.log("Calling " + frame.f_code.co_name + "(" + changes_s + ")")
         elif changes:
-            self.log(' ' * 40, '#', changes_s)
+            self.log(" " * 40, "#", changes_s)
 
-        if event == 'line':
+        if event == "line":
             try:
                 module = inspect.getmodule(frame.f_code)
                 if module is None:
                     source = inspect.getsource(frame.f_code)
                 else:
                     source = inspect.getsource(module)
-                current_line = source.split('\n')[frame.f_lineno - 1]
+                current_line = source.split("\n")[frame.f_lineno - 1]
 
             except OSError as err:
                 self.log(f"{err.__class__.__name__}: {err}")
                 current_line = ""
 
-            self.log(repr(frame.f_lineno) + ' ' + current_line)
+            self.log(repr(frame.f_lineno) + " " + current_line)
 
-        if event == 'return':
-            self.log(frame.f_code.co_name + '()' + " returns " + repr(arg))
+        if event == "return":
+            self.log(frame.f_code.co_name + "()" + " returns " + repr(arg))
             self.last_vars = {}  # Delete 'last' variables
 
 
@@ -264,10 +269,10 @@ class Debugger(Tracer):
     def execute(self, command: str) -> None:
         """Execute `command`"""
 
-        sep = command.find(' ')
+        sep = command.find(" ")
         if sep > 0:
             cmd = command[:sep].strip()
-            arg = command[sep + 1:].strip()
+            arg = command[sep + 1 :].strip()
         else:
             cmd = command.strip()
             arg = ""
@@ -279,9 +284,11 @@ class Debugger(Tracer):
     def commands(self) -> list[str]:
         """Return a list of commands"""
 
-        cmds = [method.replace('_command', '')
-                for method in dir(self.__class__)
-                if method.endswith('_command')]
+        cmds = [
+            method.replace("_command", "")
+            for method in dir(self.__class__)
+            if method.endswith("_command")
+        ]
         cmds.sort()
         return cmds
 
@@ -289,8 +296,11 @@ class Debugger(Tracer):
         """Give help on given `command`. If no command is given, give help on all"""
 
         if command:
-            possible_cmds = [possible_cmd for possible_cmd in self.commands()
-                             if possible_cmd.startswith(command)]
+            possible_cmds = [
+                possible_cmd
+                for possible_cmd in self.commands()
+                if possible_cmd.startswith(command)
+            ]
 
             if len(possible_cmds) == 0:
                 self.log(f"Unknown command {repr(command)}. Possible commands are:")
@@ -306,19 +316,20 @@ class Debugger(Tracer):
 
     def command_method(self, command: str) -> Optional[Callable[[str], None]]:
         """Convert `command` into the method to be called.
-           If the method is not found, return `None` instead."""
+        If the method is not found, return `None` instead."""
 
-        if command.startswith('#'):
+        if command.startswith("#"):
             return None  # Comment
 
-        possible_cmds = [possible_cmd for possible_cmd in self.commands()
-                         if possible_cmd.startswith(command)]
+        possible_cmds = [
+            possible_cmd for possible_cmd in self.commands() if possible_cmd.startswith(command)
+        ]
         if len(possible_cmds) != 1:
             self.help_command(command)
             return None
 
         cmd = possible_cmds[0]
-        return getattr(self, cmd + '_command')
+        return getattr(self, cmd + "_command")
 
     def step_command(self, arg: str = "") -> None:
         """Execute up to the next line"""
@@ -354,8 +365,7 @@ class Debugger(Tracer):
                 source_lines, line_number = inspect.getsourcelines(obj)
                 current_line = -1
             else:
-                source_lines, line_number = \
-                    inspect.getsourcelines(self.frame.f_code)
+                source_lines, line_number = inspect.getsourcelines(self.frame.f_code)
                 current_line = self.frame.f_lineno
         except Exception as err:
             self.log(f"{err.__class__.__name__}: {err}")
@@ -364,12 +374,12 @@ class Debugger(Tracer):
             current_line = -1
 
         for line in source_lines:
-            spacer = ' '
+            spacer = " "
             if line_number == current_line:
-                spacer = '>'
+                spacer = ">"
             elif line_number in self.breakpoints:
-                spacer = '#'
-            self.log(f'{line_number:4}{spacer} {line}', end='')
+                spacer = "#"
+            self.log(f"{line_number:4}{spacer} {line}", end="")
             line_number += 1
 
     def break_command(self, arg: str = "") -> None:
@@ -381,7 +391,7 @@ class Debugger(Tracer):
 
     def delete_command(self, arg: str = "") -> None:
         """Delete breakoint in line given by `arg`.
-           Without given line, clear all breakpoints"""
+        Without given line, clear all breakpoints"""
 
         if arg:
             try:
@@ -402,10 +412,10 @@ class Debugger(Tracer):
     def assign_command(self, arg: str) -> None:
         """Use as 'assign VAR=VALUE'. Assign VALUE to local variable VAR."""
 
-        sep = arg.find('=')
+        sep = arg.find("=")
         if sep > 0:
             var = arg[:sep].strip()
-            expr = arg[sep + 1:].strip()
+            expr = arg[sep + 1 :].strip()
         else:
             self.help_command("assign")
             return
@@ -415,4 +425,3 @@ class Debugger(Tracer):
             vars[var] = eval(expr, self.frame.f_globals, vars)
         except Exception as err:
             self.log(f"{err.__class__.__name__}: {err}")
-
